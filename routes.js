79a2d1e103a31e85794b26c054c10a4861fa333e
krushndayshmookh <krushndayshmookh@gmail.com>
2019-01-11 00:15:34 +0530

    create simple org creation routes

---
+++ routes.js
@@ -0,0 +1,55 @@
+const express = require('express')
+var router = express.Router()
+
+
+// User side pages -----
+
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+router.get('/about', (req, res) => {
+    res.render('about')
+})
+
+// -----
+
+// Admin routes -----
+
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.post('/login', (req, res) => {
+    //- console.log(req.body)
+    if (req.body.username == 'krushn' && req.body.password == 'dayshmookh') {
+        res.send(true)
+    } else {
+        res.send(false)
+    }
+
+})
+
+// -----
+
+// Data Routes -----
+
+// Require controller modules.
+var organizationController = require('./controllers/organizationController')
+
+
+
+
+
+// GET request for creating organization form.
+router.get('/organization/create', organizationController.organization_create_get)
+// POST request for creating organization.
+router.post('/organization/create', organizationController.organization_create_post)
+
+// GET request for one organization.
+router.get('/organization/:id', organizationController.organization_detail_get)
+
+
+
+//export this router to use in our index.js
+module.exports = router
