3aa719703cb5dc01271280f55d3e124a60976c9c
krushndayshmookh <krushndayshmookh@gmail.com>
2019-01-10 16:15:59 +0530

    init server

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log(process.env.PORT)
+const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
+
+/* ---------------------------------------------------- */
+
+var app = express()
+var server = http.createServer(app)
+var io = socketIO(server)
+
+/* ---------------------------------------------------- */
+
+var PORT = process.env.PORT || 3000
+
+/* ---------------------------------------------------- */
+
+app.get('/', (req, res) => {
+    res.end('Hi world!')
+})
+
+
+
+
+/* ---------------------------------------------------- */
+
+server.listen(PORT, () => {
+    console.log('Listening on port ' + PORT)
+})
