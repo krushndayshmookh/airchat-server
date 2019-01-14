de1b8ba08fe32bcd800f43a409f651ce0e0f4840
krushndayshmookh <krushndayshmookh@gmail.com>
2019-01-14 10:36:21 +0530

    new task to watch and build css during development

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, parallel } = require('gulp');
+const { src, dest, parallel, watch } = require('gulp');
 const sass = require('gulp-sass');
 const minifyCSS = require('gulp-csso');
 
@@ -14,6 +14,11 @@ function css() {
         .pipe(dest('build/css'))
 }
 
+function devCss() {
+    watch('src/scss', parallel(css));
+}
+
 exports.static = static;
 exports.css = css;
+exports.devCss = devCss;
 exports.default = parallel(css);
