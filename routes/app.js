ce59177e697f6bdbea2feedd84c920ad4b26d0b0
krushndayshmookh <krushndayshmookh@gmail.com>
2019-02-13 22:17:38 +0530

    Conversations screen mockup

--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,14 @@
 const express = require('express')
 var router = express.Router()
 
-
 // User side pages -----
 
 router.get('/', (req, res) => {
-    res.render('app/index')
+	res.render('app/index')
 })
 router.get('/organization/manage', (req, res) => {
-    res.render('app/organization/manage')
+	res.render('app/organization/manage')
 })
-
-
 
 // -----
 
@@ -21,12 +18,14 @@ router.get('/organization/manage', (req, res) => {
 var organizationController = require('../controllers/organizationController')
 
 // GET request for creating organization form.
-router.get('/organization/create', organizationController.organization_create_get)
+router.get(
+	'/organization/create',
+	organizationController.organization_create_get
+)
 // GET request for all organizations dashboard.
 router.get('/organization', organizationController.organization_get)
 
 // -----
-
 
 var userController = require('../controllers/userController')
 
@@ -35,6 +34,9 @@ router.get('/user/create', userController.user_create_get)
 // GET request for all users dashboard.
 router.get('/user', userController.user_get)
 
+router.get('/user/chats', (req, res) => {
+	res.render('app/chat/list')
+})
 
 // -----
 
