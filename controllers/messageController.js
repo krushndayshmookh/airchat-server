ba949b28d3e514338bfd198a2307050376278891
krushndayshmookh <krushndayshmookh@gmail.com>
2019-03-23 21:57:28 +0530

    Create messageController.js

---
+++ controllers/messageController.js
@@ -0,0 +1,61 @@
+const Message = require('../models/message')
+const moment = require('moment')
+
+exports.message_create_post = (req, res) => {
+	let newMessage = new Message({
+		from: req.body.from,
+		to: req.body.title,
+		data: {
+			text: req.body.text
+		},
+		time: {
+			sent: new moment()
+		}
+	})
+
+	newMessage.save(err => {
+		if (err) return res.status(500).send(err)
+
+		return res.send(newMessage)
+	})
+}
+
+exports.messages_get = (req, res) => {
+	Message.find({}, (err, result) => {
+		if (err) return res.status(500).send(err)
+
+		if (result) return res.send(result)
+
+		return res.send(false)
+	})
+}
+
+exports.message_get = (req, res) => {
+	Message.findById(req.params.id, (err, result) => {
+		if (err) return res.status(500).send(err)
+
+		if (result) return res.send(result)
+
+		return res.send(false)
+	})
+}
+
+exports.message_delete_all_get = (req, res) => {
+	Message.remove({}, (err, result) => {
+		if (err) return res.status(500).send(err)
+
+		if (result) return res.send(result)
+
+		return res.send(false)
+	})
+}
+
+exports.message_delete_post = (req, res) => {
+	Message.findByIdAndRemove(req.params.id, (err, result) => {
+		if (err) return res.status(500).send(err)
+
+		if (result) return res.send(result)
+
+		return res.send(false)
+	})
+}
