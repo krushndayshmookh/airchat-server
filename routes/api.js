421f655d1ce5963187ba5107f78152ddc561ecf3
krushndayshmookh <krushndayshmookh@gmail.com>
2019-03-23 22:03:27 +0530

    organization API routes

--- routes/api.js
+++ routes/api.js
@@ -18,13 +18,16 @@ var organizationController = require('../controllers/organizationController')
 router.post('/organization/create', organizationController.organization_create_post)
 
 // GET request for one organization.
-router.get('/organization/:id', organizationController.organization_detail_get)
+router.get('/organization/:id', organizationController.organization_get)
 
 // POST request for one organization.
 router.post('/organization/:id/delete', organizationController.organization_delete_post)
 
 // GET request for all organizations.
 router.get('/organizations', organizationController.organizations_get)
+
+// GET request for all root organizations.
+router.get('/organizations/root', organizationController.organizations_root_get)
 
 // GET request for delete all organizations.
 router.get('/organizations/delete/all', organizationController.organizations_delete_all_get)
