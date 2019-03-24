9e5b30eed56bcfe8648df03a3f64190c811707d1
krushndayshmookh <krushndayshmookh@gmail.com>
2019-03-24 08:50:25 +0530

    create conversationController.js

---
+++ controllers/conversationController.js
@@ -0,0 +1,78 @@
+const Conversation = require('../models/conversation')
+const moment = require('moment')
+
+exports.conversation_create_post = (req, res) => {
+	let newConversation = new Conversation({
+		name: req.body.name,
+		creator: req.body.creator
+	})
+
+	newConversation.admins.push(req.body.creator)
+	newConversation.users.push(req.body.creator)
+
+	newConversation.save().exec(err => {
+		if (err) return res.status(500).send(err)
+
+		return res.send(newConversation)
+	})
+}
+
+exports.conversations_get = (req, res) => {
+	Conversation.find({}).exec((err, result) => {
+		if (err) return res.status(500).send(err)
+
+		if (result) return res.send(result)
+
+		return res.send(false)
+	})
+}
+
+exports.conversations_from_user_get = (req, res) => {
+	Conversation.find({ from: req.params.from }).exec((err, result) => {
+		if (err) return res.status(500).send(err)
+
+		if (result) return res.send(result)
+
+		return res.send(false)
+	})
+}
+
+exports.conversations_to_user_get = (req, res) => {
+	Conversation.find({ to: req.params.to }).exec((err, result) => {
+		if (err) return res.status(500).send(err)
+
+		if (result) return res.send(result)
+
+		return res.send(false)
+	})
+}
+
+exports.conversation_get = (req, res) => {
+	Conversation.findById(req.params.id).exec((err, result) => {
+		if (err) return res.status(500).send(err)
+
+		if (result) return res.send(result)
+
+		return res.send(false)
+	})
+}
+
+exports.conversations_delete_all_get = (req, res) => {
+	Conversation.remove({}).exec((err, result) => {
+		if (err) return res.status(500).send(err)
+
+		if (result) return res.send(result)
+
+		return res.send(false)
+	})
+}
+
+exports.conversation_delete_post = (req, res) => {
+	Conversation.findByIdAndRemove(req.params.id).exec((err, result) => {
+		if (err) return res.status(500).send(err)
+
+		if (result) return res.send(result)
+
+		return res.send(false)
+	})
+}
