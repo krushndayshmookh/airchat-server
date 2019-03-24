1e12d19b0c64d6a390e19393fbc7d7d0c76a264e
krushndayshmookh <krushndayshmookh@gmail.com>
2019-03-24 08:43:45 +0530

    beautify

--- models/organization.js
+++ models/organization.js
@@ -2,34 +2,36 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const organizationSchema = new Schema({
-    name: {
-        type: String
-    },
-    location: {
-        name: {
-            type: String
-        },
-        geo: {
-            lat: {
-                type: Number
-            },
-            long: {
-                type: Number
-            }
-        }
-    },
-    parent: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Organization'
-    },
-    root: {
-        type: Boolean,
-        default: true
-    },
-    suborgs: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Organization'
-    }]
+	name: {
+		type: String
+	},
+	location: {
+		name: {
+			type: String
+		},
+		geo: {
+			lat: {
+				type: Number
+			},
+			long: {
+				type: Number
+			}
+		}
+	},
+	parent: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Organization'
+	},
+	root: {
+		type: Boolean,
+		default: true
+	},
+	suborgs: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Organization'
+		}
+	]
 })
 
 module.exports = mongoose.model('Organization', organizationSchema)
