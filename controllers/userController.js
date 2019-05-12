e7c6e1ab7c7573eb4ff0c1d87330e7d7b1488079
sahilkindarle <sahilkindarle212@gmail.com>
2019-05-12 16:36:32 +0530

    controller edits result and trial page renderring

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,92 +1,92 @@
-const User = require('../models/user')
-const Organization = require('../models/organization')
+const User = require("../models/user");
+const Organization = require("../models/organization");
 
 // API -----
 // GET USER BY ID
 exports.user_get = (req, res) => {
-	User.findById(req.params.id)
-		.populate('organization')
-		.exec((err, result) => {
-			if (err) return res.status(500).send(err)
+  User.findById(req.params.id)
+    .populate("organization")
+    .exec((err, result) => {
+      if (err) return res.status(500).send(err);
 
-			if (result) return res.send(result)
+      if (result) return res.render("app/user/trial", { result });
 
-			return res.send('No record found for id ' + req.params.id)
-		})
-}
+      return res.send("No record found for id " + req.params.id);
+    });
+};
 
 // GET ALL USERS
 exports.users_get = (req, res) => {
-	User.find({}).exec((err, result) => {
-		if (err) return res.status(500).send(err)
+  User.find({}).exec((err, result) => {
+    if (err) return res.status(500).send(err);
 
-		if (result) return res.send(result)
+    if (result) return res.send(result);
 
-		return res.send('No record found.')
-	})
-}
+    return res.send("No record found.");
+  });
+};
 
 // CREATE POST
 exports.user_create_post = (req, res) => {
-	let newUser = new User({
-		name: req.body.name,
-		username: req.body.username,
-		password: req.body.password,
-		position: req.body.position,
-		organization: req.body.organization
-	})
+  let newUser = new User({
+    name: req.body.name,
+    username: req.body.username,
+    password: req.body.password,
+    position: req.body.position,
+    organization: req.body.organization
+  });
 
-	newUser.save(err => {
-		if (err) return res.status(500).send(err)
+  newUser.save(err => {
+    if (err) return res.status(500).send(err);
 
-		return res.send(newUser)
-	})
-}
+    return res.send(newUser);
+  });
+};
 
 // DELETE ALL USERS
 exports.users_delete_all_get = (req, res) => {
-	User.deleteMany({}).exec((err, result) => {
-		if (err) return res.status(500).send(err)
+  User.deleteMany({}).exec((err, result) => {
+    if (err) return res.status(500).send(err);
 
-		if (result) return res.send(result)
+    if (result) return res.send(result);
 
-		return res.send(false)
-	})
-}
+    return res.send(false);
+  });
+};
 
-// DELETE POST BY ID 
+// DELETE POST BY ID
 exports.user_delete_post = (req, res) => {
-	User.findByIdAndDelete(req.params.id).exec((err, result) => {
-		if (err) return res.status(500).send(err)
+  User.findByIdAndDelete(req.params.id).exec((err, result) => {
+    if (err) return res.status(500).send(err);
 
-		if (result) return res.send(result)
+    if (result) return res.send(result);
 
-		return res.send(false)
-	})
-}
+    return res.send(false);
+  });
+};
 
 // Application -----
 
 // VIEW ALL USERS
 exports.users_view_get = (req, res) => {
-	User.find({})
-		.populate('organization')
-		.exec((err, result) => {
-			if (err) return res.status(500).send(err)
+  User.find({})
+    .populate("organization")
+    .exec((err, result) => {
+      if (err) return res.status(500).send(err);
 
-			if (result) return res.render('app/user/index', { users: result })
+      if (result) return res.render("app/user/index", { users: result });
 
-			return res.send('No record found.')
-		})
-}
+      return res.send("No record found.");
+    });
+};
 // CREATE USER VIEW
 exports.user_create_view_get = (req, res) => {
-	Organization.find({}).exec((err, result) => {
-		if (err) return res.status(500).send(err)
+  Organization.find({}).exec((err, result) => {
+    if (err) return res.status(500).send(err);
 
-		if (result) return res.render('app/user/create', { organizations: result })
+    if (result) return res.render("app/user/create", { organizations: result });
 
-		return res.send('No record found.')
-	})
-	//- res.render('user/create')
-}
+    return res.send("No record found.");
+  });
+  //- res.render('user/create')
+};
